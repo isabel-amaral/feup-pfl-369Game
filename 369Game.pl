0d6ef8cb0d1fc4a32295a275f91dcec4359d7d31
Milena Gouveia <up202008862@g.uporto.pt>
2022-12-19 15:31:31 +0000

    Implemented insert_piece

--- 369Game.pl
+++ 369Game.pl
@@ -24,3 +24,14 @@ read_move_until_valid(Size, Move):- repeat,
                                     Column =< Size-1,
                                     Move = [Row, Column],
                                     !.              
+
+
+insert_piece([_ | Rest], Elem, 0, [Elem | Rest]) :- !.
+insert_piece([Member | Rest], Elem, Position, Result) :- Position1 is Position-1,
+                                                         insert_piece(Rest, Elem, Position1, R1),
+                                                         Result = [Member | R1].
+
+insert_piece([Line | Rest], Elem, 0, Col, NewBoard) :- insert_piece(Line, Elem, Col, NewLine), NewBoard = [NewLine | Rest], !.
+insert_piece([Line | Rest], Elem, L, Col, NewBoard) :- Line1 is L-1,
+                                                       insert_piece(Rest, Elem, Line1, Col, NB1),
+                                                       NewBoard = [Line | NB1].
