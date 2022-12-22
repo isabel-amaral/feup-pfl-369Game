f6df41d56faa46e13a8db029bf8979b302bd550a
Isabel Amaral <isabel.andre.amaral@gmail.com>
2022-12-22 15:53:39 -0100

    Fixed identations

--- 369Game.pl
+++ 369Game.pl
@@ -1,37 +1,45 @@
 /*
-read_move(Line, Column) :- read(N-L),
-                           number(N),
-                           is_alpha(L),
-                           Line is N-1,
-                           string_upper(L, L1)
-                           char_code('A', A),
-                           char_code(L1, Letter)
-                           Column is Letter-A.
+read_move(Line, Column) :- 
+    read(N-L),
+    number(N),
+    is_alpha(L),
+    Line is N-1,
+    string_upper(L, L1)
+    char_code('A', A),
+    char_code(L1, Letter)
+    Column is Letter-A.
 */
 
-read_move(Row, Column) :- read(R-C),
-                           number(R),
-                           number(C),
-                           Row is R-1,
-                           Column is C-1.
+read_move(Row, Column) :- 
+    read(R-C),
+    number(R),
+    number(C),
+    Row is R-1,
+    Column is C-1.
 
-read_move_until_valid(Size, Move):- repeat,
-                                    write('Insert your move in Row-Column format'), nl,
-                                    read_move(Row, Column),
-                                    Row >= 0,
-                                    Row =< Size-1,
-                                    Column >= 0,
-                                    Column =< Size-1,
-                                    Move = [Row, Column],
-                                    !.              
+read_move_until_valid(Size, Move):- 
+    repeat,
+    write('Insert your move in Row-Column format'), nl,
+    read_move(Row, Column),
+    Row >= 0,
+    Row =< Size-1,
+    Column >= 0,
+    Column =< Size-1,
+    Move = [Row, Column],
+    !.              
 
 
 insert_piece([_ | Rest], Piece, 0, [Piece | Rest]) :- !.
-insert_piece([Member | Rest], Piece, Position, Result) :- Position1 is Position-1,
-                                                          insert_piece(Rest, Piece, Position1, R1),
-                                                          Result = [Member | R1].
+insert_piece([Member | Rest], Piece, Position, Result) :- 
+    Position1 is Position-1,
+    insert_piece(Rest, Piece, Position1, R1),
+    Result = [Member | R1].
 
-insert_piece([Row | Rest], Piece, 0, Col, NewBoard) :- insert_piece(Row, Piece, Col, NewRow), NewBoard = [NewRow | Rest], !.
-insert_piece([Row | Rest], Piece, R, Col, NewBoard) :- Row1 is R-1,
-                                                       insert_piece(Rest, Piece, Row1, Col, NB1),
-                                                       NewBoard = [Row | NB1].
+insert_piece([Row | Rest], Piece, 0, Col, NewBoard) :- 
+    insert_piece(Row, Piece, Col, NewRow), 
+    NewBoard = [NewRow | Rest], 
+    !.
+insert_piece([Row | Rest], Piece, R, Col, NewBoard) :- 
+    Row1 is R-1,
+    insert_piece(Rest, Piece, Row1, Col, NB1),
+    NewBoard = [Row | NB1].
