1037f078cd7cb6e6fc75f7e52c3e3480da26dff8
Milena Gouveia <up202008862@g.uporto.pt>
2022-12-30 03:22:53 +0000

    Fixed value predicate call on game_over

--- input.pl
+++ input.pl
@@ -1,21 +1,10 @@
-/*
-read_move(Line, Column) :- 
+read_move(Row, Column) :- 
     read(N-L),
     number(N),
-    is_alpha(L),
-    Line is N-1,
-    string_upper(L, L1)
-    char_code('A', A),
-    char_code(L1, Letter)
+    Row is N-1,
+    char_code('a', A),
+    char_code(L, Letter),
     Column is Letter-A.
-*/
-
-read_move(Row, Column) :- 
-    read(R-C),
-    number(R),
-    number(C),
-    Row is R-1,
-    Column is C-1.
 
 read_move_until_valid(ListOfValidMoves, Move):- 
     repeat,
