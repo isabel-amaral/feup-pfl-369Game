2bebde3dcdbbb3b1299928a43e8695ca9dd76b2a
Milena Gouveia <up202008862@g.uporto.pt>
2022-12-30 20:30:51 +0000

    Implemented choose_game_type

--- 369Game.pl
+++ 369Game.pl
@@ -33,6 +33,26 @@ initial_state(Size, [Size, Board, 0, 0, w]) :-
     create_board(Size, Size, Board).
 
 
+%map_option_to_game_type(+Option, -GameType)
+map_option_to_game_type(1, h/h).
+map_option_to_game_type(2, h/pc).
+map_option_to_game_type(3, pc/pc).
+
+
+%choose_game_type(-GameType)
+choose_game_type(GameType) :- 
+    repeat, nl,
+    write('Choose the game type: '), nl,
+    write('1 - Human-Human (H/H)'),nl,
+    write('2 - Human-Computer (H/PC)'), nl,
+    write('3 - Computer-Computer (PC/PC)'), nl,
+    read(N),
+    number(N),
+    N >= 1,
+    N =< 3,
+    map_option_to_game_type(N, GameType).
+
+
 %display_winner(+Winner)
 display_winner(b) :- write('The black pieces won the victory in this battle of wits!'), nl.
 display_winner(w) :- write('The white pieces won the victory in this battle of wits!'), nl.
