d6378781cfcfaa10ef5a0570c49c7b9de5031e52
Isabel Amaral <isabel.andre.amaral@gmail.com>
2023-01-02 01:09:29 -0100

    Added description of some of the points.pl predicates

--- points.pl
+++ points.pl
@@ -1,4 +1,5 @@
 % count_pieces_in_line(+Line, +Player, -Number)
+% Counts the number of pieces of a given colour in a given line
 count_pieces_in_line([], _, 0).
 count_pieces_in_line([Player | Rest], Player, Number) :-
     count_pieces_in_line(Rest, Player, NumberAux),
@@ -48,6 +49,7 @@ diagonal2_points(Board, Player, Move, Size, Points) :-
 
 
 % value_line(+Line, +Player, -Value)
+% Determines how many points a move in the given sequence, i.e. line, column or diagonal is worth
 value_line(Line, Player, Value) :-
     count_pieces_in_line(Line, Player, Number),
     Number = 2,
@@ -69,6 +71,7 @@ value_line(Line, Player, Value) :-
 value_line(_, _, 0).
 
 % value_lines(+Board, -Line, +Player, -Points)
+% Determines which board line can be used to achieve the highest number of points in the current game cycle iteration
 value_lines(Board, BestLine, Player, Points) :- 
     value_lines(Board, BestLine, 0, Player, Points).
 value_lines([], BestLine, LineCounter, _, 0) :-
@@ -86,6 +89,7 @@ value_lines([Line | _], BestLine, LineCounter, Player, Points) :-
     BestLine is LineCounter.
 
 % value_columns(+Board, +BoardSize, -Column, +Player, -Points)
+% Determines which board column can be used to achieve the highest number of points in the current game cycle iteration
 value_columns(Board, BoardSize, BestColumn, Player, Points) :-
     value_columns(Board, BoardSize, BestColumn, 0, Player, Points).
 value_columns(_, BoardSize, BestColumn, BoardSize, _, 0) :-
@@ -106,6 +110,8 @@ value_columns(Board, _, BestColumn, ColumnCounter, Player, Points) :-
     BestColumn is ColumnCounter.
 
 % choose_best_diagonal(+Diagonal1, +Diagonal2, +Player, -Dir, -Points)
+% Determines which diagonal of a given position guarantees more points:
+% either the diagonal that goes left to right or the one that goes right to left
 choose_best_diagonal(Diagonal1, Diagonal2, Player, Dir, Points) :-
     value_line(Diagonal1, Player, PointsAux1),
     value_line(Diagonal2, Player, PointsAux2),
@@ -118,6 +124,7 @@ choose_best_diagonal(_, Diagonal2, Player, Dir, Points) :-
     Dir = d2.
 
 % value_diagonals_aux(+Position1, +Points1, +Position2, +Points2, -Position, -Points)
+% Given two diagonals starting at different positions, determines which one guaranteees more points
 value_diagonals_aux(Position1, Points1, _, Points2, Position, Points) :-
     Points1 >= Points2,
     Points is Points1,
@@ -128,6 +135,7 @@ value_diagonals_aux(_, _, Position2, Points2, Position, Points) :-
     Position = Position2.
 
 % value_diagonals(+Board, +BoardSize, -Position, +Player, -Points)
+% Determines which board diagonal can be used to achieve the highest number of points in the current game cycle iteration
 value_diagonals(Board, BoardSize, BestPosition, Player, Points) :-
     value_diagonals(Board, BoardSize, BestPosition, 0, Player, Points).
 value_diagonals(_, BoardSize, BestPosition, BoardSize, _, 0) :-
@@ -142,6 +150,7 @@ value_diagonals(Board, BoardSize, BestPosition, PositionCounter, Player, Points)
 
 
 % value(+GameState, +Player, -Value)
+% Determines the pontuation of a given player at a certain moment
 value(GameState, w, Value) :-
     get_white_player_pontuation(GameState, Value).
 value(GameState, b, Value) :-
