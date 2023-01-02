23924da97ada3c37dba0b33a174e2b6d7e8ae864
Milena Gouveia <up202008862@g.uporto.pt>
2023-01-02 14:28:00 +0000

    Added description of some functions in menu.pl

--- menu.pl
+++ menu.pl
@@ -4,6 +4,7 @@ map_option_to_game_type(2, h/pc).
 map_option_to_game_type(3, pc/pc).
 
 % choose_game_type(-GameType)
+% Asks the user to choose the type of game by selecting a number between 1 and 3.
 choose_game_type(GameType) :- 
     repeat, nl,
     write('Choose the game type: '), nl,
@@ -17,6 +18,8 @@ choose_game_type(GameType) :-
     map_option_to_game_type(N, GameType).
 
 % choose_level(+GameType, -Level)
+% Asks the user to choose the game level. If the game type is Human-Human (h/h), the level is automatically set to 1.
+% Otherwise, the user is repeatedly asked to insert a level between 1 and 2.
 choose_level(h/h, 1) :- !.
 
 choose_level(_, Level) :-
@@ -29,6 +32,7 @@ choose_level(_, Level) :-
     Level is L.
 
 % choose_board_size(-Size)
+% Repeatedly asks the user to insert the board size until a number is given.
 choose_board_size(Size) :-
     repeat, nl,
     write('Choose board size:'), nl,
@@ -37,6 +41,7 @@ choose_board_size(Size) :-
     Size is S.
 
 % menu(-GameType, -Level, -Size)
+% Displays the game menu, which allows the user to choose the level and type of game, as well as the board size.
 menu(GameType, Level, Size) :- 
     nl, 
     choose_game_type(GameType),
