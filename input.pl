b7944a9239f43ad6804eaf5cfafece790161b8a2
Milena Gouveia <up202008862@g.uporto.pt>
2023-01-02 14:27:49 +0000

    Added description of some functions in input.pl

--- input.pl
+++ input.pl
@@ -1,4 +1,8 @@
 % read_move(-Line, -Column)
+% Reads the player's move in N-L format, where N corresponds to a board line and should be a number
+% and L corresponds to a board column and should be a letter.
+% The Line variable is obtained by subtracting 1 from N and the Column variable is
+% the result of subtracting the ASCII code of 'a' from the ASCII code of L
 read_move(Line, Column) :- 
     read(N-L),
     number(N),
@@ -8,6 +12,8 @@ read_move(Line, Column) :-
     Column is Letter-A.
 
 % read_move_until_valid(+ListOfValidMoves, -Move)
+% Repeatedly asks the user's next move until a valid one is inserted, i.e., the move is a member of
+% ListOfValidMoves, if so, the chosen move is returned in the Move variable, in [Line, Column] format 
 read_move_until_valid(ListOfValidMoves, Move):- 
     repeat, nl,
     write('Insert your move in Number-Letter format'), nl,
